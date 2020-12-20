099bfbf2cbce502128339bdb327c60bbd25ccd97
Tristan Bond <tristanebond@gmail.com>
2020-12-20 13:45:42 -0800

    readme file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# noteTaker
+# Note Taker
+
+  ## Description
+    This projects is an application that allows user to write and save notes. This is done with a Heroku server that uses get and post requests to save data to a json database stored in the project. Api routes are used to gather and send data tot he user.
+
+  ## Table of Contents
+
+  *[Installation](#installation)
+
+  *[Usage](#usage)
+
+  *[Tests](#tests)
+
+  *[Questions](#questions)
+
+  ## Installation
+  npm i will install all dependencies
+
+  ## Usage
+  the project can be found on a heroku domain: https://guarded-shelf-48388.herokuapp.com/ or run through node and localhost
+
+
+  ## Tests
+  node server to use local host or through the heroku domain
+
+  ## Questions
+  tbond4
+  tristanebond@gmail.com
+  
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const fs =require("fs");
-const { BADFAMILY } = require("dns");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,25 +11,26 @@ app.use(express.json());
 
 
 app.get("/api/notes",function(req,res){
-  // should read the db.json file and return all saved notes as JSON. use fs .THEN JSON.parse() and res.json()
   let db;
+
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
     db = JSON.parse(data);
     res.json(db);
   });
-  
+
 });
 
 app.post("/api/notes", function(req,res){
-  // Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.  fs read file,parse it to
-  // arrayobject to work with, push new note,JSON.stringify, fs append
   let newNote=req.body;
   let db;
+
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
+
     db = JSON.parse(data);
     db.push(newNote);
+
     for(let i=0;i<db.length;i++){
       db[i].id=i;
     }
@@ -46,7 +46,6 @@ app.post("/api/notes", function(req,res){
 app.delete("/api/notes/:id", function(req,res){
 let ID=req.params.id;
 let db;
-
 let bd = [];
   fs.readFile("./db/db.json", (err, data) => {
     if (err) throw err;
@@ -64,26 +63,15 @@ let bd = [];
       console.log('The Note has been deleted!');
     });
     res.end("Success");
-});
-//findindex
-//remove with splice
+  });
 
-// or filter
-//myarray = myarray.filter(element =>{element.id !== req.params.id})
-//fs write
-// return sucess messeg
 });
 
 app.get("/notes", function(req, res) {
-
     res.sendFile(path.join(__dirname,"./public/notes.html"));
   });
 
-// DELETE /api/notes/:id - Should receive a query parameter containing the id of a note to delete. 
-// This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note,
-//  you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 app.get("*", function(req, res) {
-
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
